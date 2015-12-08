456602d924e1f5e2d815b6924e593f729458b146
lubin.xuan <lubin.xuan@gmail.com>
2015-12-08 18:09:16 +0800

    solr 插件 日志 级别调整

--- src/main/java/me/robin/api/json/JsonExtractor.java
+++ src/main/java/me/robin/api/json/JsonExtractor.java
@@ -18,6 +18,9 @@ import java.util.List;
  * ie.
  */
 public class JsonExtractor extends DataExtractor {
+
+    private static final Path.Builder builder = new Path.Builder();
+
     @Override
     public List<JSONObject> readData(String source, Context context) {
         String json = JsonDataUtil.read(source);
@@ -43,25 +46,24 @@ public class JsonExtractor extends DataExtractor {
     private List<JSONObject> getMapsFromJson(Context context, Object data) {
         if (null != data && null != context && !context.isEmpty()) {
             List<JSONObject> dataList = new ArrayList<JSONObject>();
-            Path.Builder builder = new Path.Builder();
             if (null != context.getTarget() && context.getTarget().length() > 0) {
                 Object dataArr = PathReader.read(data, builder.eval(context.getTarget()));
                 if (dataArr instanceof JSONArray) {
-                    ((JSONArray) dataArr).forEach(item -> read(item, context, dataList, builder));
+                    ((JSONArray) dataArr).forEach(item -> read(item, context, dataList));
                 } else {
-                    read(dataArr, context, dataList, builder);
+                    read(dataArr, context, dataList);
                 }
             } else if (data instanceof JSONArray) {
-                ((JSONArray) data).forEach(item -> read(item, context, dataList, builder));
+                ((JSONArray) data).forEach(item -> read(item, context, dataList));
             } else {
-                read(data, context, dataList, builder);
+                read(data, context, dataList);
             }
             return dataList;
         }
         return null;
     }
 
-    private void read(Object data, Context context, List<JSONObject> container, Path.Builder builder) {
+    private void read(Object data, Context context, List<JSONObject> container) {
         JSONObject result = new JSONObject();
         for (Entity entity : context.getEntityList()) {
             if (entity instanceof CombineEntity) {
