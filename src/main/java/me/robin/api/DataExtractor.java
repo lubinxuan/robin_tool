7db503c0f085ff41b46d3730f2b87cbf8552741e
xuanlubin <xlb900907>
2016-08-02 10:53:56 +0800

    solrcloud 查询 shard 支持

--- src/main/java/me/robin/api/DataExtractor.java
+++ src/main/java/me/robin/api/DataExtractor.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 /**
  * Created by Lubin.Xuan on 2015/6/11.
@@ -21,30 +22,44 @@ public abstract class DataExtractor {
     public abstract List<JSONObject> readData(String source, Context context);
 
     private static final Map<Type, DataExtractor> DATA_EXTRACTOR_MAP = new ConcurrentHashMap<>();
-    private final static DataExtractor json = new JsonExtractor();
 
-    static {
-        DATA_EXTRACTOR_MAP.put(Type.JSON, json);
-        DATA_EXTRACTOR_MAP.put(Type.JSONP, json);
-        DATA_EXTRACTOR_MAP.put(Type.XML, new XmlExtractor());
+    private static DataExtractor get(Type type) {
+        return DATA_EXTRACTOR_MAP.computeIfAbsent(type, type1 -> {
+            if (Type.JSON.equals(type1)) {
+                return new JsonExtractor();
+            }
+            if (Type.JSONP.equals(type1)) {
+                return new JsonExtractor();
+            }
+            if (Type.XML.equals(type1)) {
+                return new XmlExtractor();
+            }
+            return null;
+        });
     }
 
     public static List<JSONObject> readData(Type type, String source, Context context) {
-        if (DATA_EXTRACTOR_MAP.containsKey(type)) {
-            List<JSONObject> readData = DATA_EXTRACTOR_MAP.get(type).readData(source, context);
-            return null == readData ? Collections.emptyList() : readData;
-        } else {
-            return Collections.emptyList();
-        }
+        List<JSONObject> readData = get(type).readData(source, context);
+        return null == readData ? Collections.emptyList() : readData;
     }
 
+
     public static List<JSONObject> readJSON(JSON source, Context context) {
-        List<JSONObject> readData = ((JsonExtractor) json)._readJSON(source, context);
+        List<JSONObject> readData = ((JsonExtractor) get(Type.JSON))._readJSON(source, context);
         return null == readData ? Collections.emptyList() : readData;
     }
 
     public static JSONObject readJSONFirst(JSON source, Context context) {
-        List<JSONObject> readData = ((JsonExtractor) json)._readJSON(source, context);
+        List<JSONObject> readData = readJSON(source, context);
+        return null != readData && !readData.isEmpty() ? readData.get(0) : new JSONObject();
+    }
+
+    public static List<JSONObject> readJSON(String source, Context context) {
+        return get(Type.JSON).readData(source, context);
+    }
+
+    public static JSONObject readJSONFirst(String source, Context context) {
+        List<JSONObject> readData = readJSON(source, context);
         return null != readData && !readData.isEmpty() ? readData.get(0) : new JSONObject();
     }
 
